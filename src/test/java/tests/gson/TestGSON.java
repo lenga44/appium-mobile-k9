5fb5422781cfb943216595c72dbc47b80c1d458e
Nga lee <lenga@Ngas-MacBook-Pro.local>
2022-08-05 01:59:13 +0700

    data object builder

--- src/test/java/tests/gson/TestGSON.java
+++ src/test/java/tests/gson/TestGSON.java
@@ -1,17 +1,16 @@
 package tests.gson;
 
 import com.google.gson.Gson;
-import test_data_models.LoginCred;
-import tests.authen.LoginTestWithDataProvider;
+import test_data.models.LoginCred;
 
 public class TestGSON {
     public static void main(String[] args) {
-        LoginCred loginCred =new LoginCred("teo@sth.com","12345678");
+        LoginCred loginCred = new LoginCred("teo@sth.com","12345678");
         Gson gson = new Gson();
         System.out.println("Object to json: " +gson.toJson(loginCred));
 
         String loginDataJson = "{\"email\":\"teo@sth.com\",\"password\":\"12345678\"}";
         LoginCred convertFromJson = gson.fromJson(loginDataJson, LoginCred.class);
-        System.out.println("Json to Object: " +convertFromJson.toString());
+        System.out.print("Json to Object: " +convertFromJson);
     }
 }
